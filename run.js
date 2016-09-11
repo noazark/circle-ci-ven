da0eb627c2656a31a42f5969153664dc3b9e52ee
Noah H. Smith <noah@batteredoats.com>
2016-09-11 16:58:47 -0700

    unifying implementation with Node.js

---
+++ run.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+
+const expected = '6.5.0';
+const version = process.version;
+
+assert(version.indexOf(expected) >= 0, `expected python version to be ${version}, but is actually ${expected}`);
