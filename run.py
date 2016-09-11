4ca3f0d38f01af3d85a4ef4a5eb4410a1db3d43f
Noah H. Smith <noah@batteredoats.com>
2016-09-10 21:29:45 -0700

    adding a useful assertion message

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 import sys
 
-assert sys.version_info >= (2, 7, 12)
+expected = '2.7.12'
+version = '.'.join(str(x) for x in sys.version_info[0:3])
+
+assert version == expected, 'expected python version to be %s, but is actually %s' % (expected, version)
