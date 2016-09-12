7fcf92cdb1014bf55048c46662ca455733a19718
Noah H. Smith <noah@batteredoats.com>
2016-09-11 17:30:06 -0700

    im kinda terrible at this js thing

--- run.js
+++ run.js
@@ -3,4 +3,4 @@ const assert = require('assert');
 const expected = '6.5.0';
 const version = process.version;
 
-assert(version.indexOf(expected) >= 0, 'expected python version to be ' + version + ', but is actually ' + expected);
+assert(version.indexOf(expected) >= 0, 'expected node.js version to be ' + expected + ', but is actually ' + version);
